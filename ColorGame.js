16a653b9c8aac7a800837a1e4814279b9dc93436
Meghna Banerjee <mebanerj@ucsc.edu>
2018-11-08 12:49:19 -0800

    Added "easy" and "hard"

--- ColorGame.js
+++ ColorGame.js
@@ -1,32 +1,80 @@
 
-var squareColors = generateRandomColor(6);
-
+var squareLength = 6;
+var squareColors = generateRandomColor(squareLength);
 var squares = document.querySelectorAll(".square");
 var colorDisplay = document.getElementById("colorDisplay");
 var message = document.getElementById("message");
 var rightAnswer = pickColor();
 var h1 = document.querySelector("h1");
+var reset = document.querySelector("#reset");
+var easy = document.querySelector("#easy");
+var hard = document.querySelector("#hard");
 colorDisplay.textContent = rightAnswer;
-for(var i = 0; i < squares.length; i++)
+setUpSquares();
+
+reset.addEventListener("click" , function(){
+  h1.style.backgroundColor = "#232323";
+  squareColors = generateRandomColor(squareLength);
+  rightAnswer = pickColor();
+  reset.textContent = "New Colors";
+  setUpSquares();
+});
+
+easy.addEventListener("click" , function(){
+  reset.textContent = "New Colors";
+  h1.style.backgroundColor = "#232323";
+  squareLength = 3;
+  squareColors = generateRandomColor(squareLength);
+  rightAnswer = pickColor();
+
+  for(var i = squareLength; i < 6; i++)
+  {
+    squares[i].style.backgroundColor = "#232323";
+    squares[i].removeEventListener("click", squareEvent, false);
+  }
+  setUpSquares();
+
+});
+
+hard.addEventListener("click" , function(){
+  squareLength = 6;
+  h1.style.backgroundColor = "#232323";
+  reset.textContent = "New Colors";
+  squareColors = generateRandomColor(squareLength);
+  rightAnswer = pickColor();
+  setUpSquares();
+
+});
+
+function setUpSquares()
 {
-  squares[i].style.backgroundColor = squareColors[i];
-  squares[i].addEventListener( "click", function(){
+  for(var i = 0; i < squareLength; i++)
+  {
+    squares[i].style.backgroundColor = squareColors[i];
+    squares[i].addEventListener( "click", squareEvent);
+  }
+}
+
+function squareEvent()
+{
   if(this.style.backgroundColor === rightAnswer)
   {
     message.textContent = "Correct!";
     changeColor(rightAnswer);
     h1.style.backgroundColor = rightAnswer;
+    reset.textContent = "Play Again?"
   }
   else {
     message.textContent = "Try Again!";
     this.style.backgroundColor = "#232323";
   }
-  });
 }
+
+
 
 function changeColor(rightAnswer)
 {
-  for(var i = 0; i < squares.length; i++)
+  for(var i = 0; i < squareLength; i++)
   {
     squares[i].style.backgroundColor = rightAnswer;
   }
@@ -34,7 +82,7 @@ function changeColor(rightAnswer)
 
 function pickColor()
 {
-  var random = Math.floor(Math.random() * squares.length);
+  var random = Math.floor(Math.random() * squareLength);
   return squareColors[random];
 }
 
