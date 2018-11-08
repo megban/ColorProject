0e67501e81274485020684e554956ceab769036a
Meghna Banerjee <mebanerj@ucsc.edu>
2018-11-08 11:41:04 -0800

    generates random colors now

--- ColorGame.js
+++ ColorGame.js
@@ -1,26 +1,21 @@
 
-var squareColors = [
-  "rgb(50, 100, 0)",
-  "rgb(50, 255, 0)",
-  "rgb(255, 100, 0)",
-  "rgb(60, 0, 0)",
-  "rgb(150, 100, 150)",
-  "rgb(50, 100, 200)"
-]
+var squareColors = generateRandomColor(6);
 
 var squares = document.querySelectorAll(".square");
 var colorDisplay = document.getElementById("colorDisplay");
 var message = document.getElementById("message");
 var rightAnswer = pickColor();
+var h1 = document.querySelector("h1");
 colorDisplay.textContent = rightAnswer;
 for(var i = 0; i < squares.length; i++)
 {
   squares[i].style.backgroundColor = squareColors[i];
   squares[i].addEventListener( "click", function(){
-  if(this.style.backgroundColor == rightAnswer)
+  if(this.style.backgroundColor === rightAnswer)
   {
     message.textContent = "Correct!";
     changeColor(rightAnswer);
+    h1.style.backgroundColor = rightAnswer;
   }
   else {
     message.textContent = "Try Again!";
@@ -41,4 +36,26 @@ function pickColor()
 {
   var random = Math.floor(Math.random() * squares.length);
   return squareColors[random];
+}
+
+function generateRandomColor(n)
+{
+  var arr = [];
+  for(var i = 0; i < n; i++)
+  {
+    arr.push(randomColor());
+  }
+  return arr;
+}
+
+function randomColor()
+{
+  var  r = Math.floor(Math.random() * 256);
+  var  g = Math.floor(Math.random() * 256);
+  var  b = Math.floor(Math.random() * 256);
+
+  //rgb(r, g, b)
+  return "rgb(" + r + ", " + g + ", " + b + ")";
+
+
 }
